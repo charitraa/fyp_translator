37325479a1c62284552c2381558ae68fc48c2ed1
charitra shrestha <stharabi9862187405@gmail.com>
2025-12-03 00:03:41 +0545

    update the model

--- translator/translator.py
+++ translator/translator.py
@@ -1,42 +1,34 @@
-# translator/translator.py
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from IndicTransToolkit.processor import IndicProcessor
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, MBart50TokenizerFast, MBartForConditionalGeneration, pipeline
 import torch
 
 class LocalTranslator:
     def __init__(self, model_dir="models"):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.ip = IndicProcessor(inference=True)
+        print(f"Using device: {self.device}")
 
-        # Load models
-        self.ne2en_tokenizer = AutoTokenizer.from_pretrained(f"{model_dir}/ne2en", trust_remote_code=True)
-        self.ne2en_model = AutoModelForSeq2SeqLM.from_pretrained(f"{model_dir}/ne2en", trust_remote_code=True).to(self.device)
+        mbart_path = f"{model_dir}/ne2en"
+        self.mbart_tokenizer = MBart50TokenizerFast.from_pretrained(mbart_path)
+        self.mbart_model = MBartForConditionalGeneration.from_pretrained(mbart_path).to(self.device)
 
-        self.en2ne_tokenizer = AutoTokenizer.from_pretrained(f"{model_dir}/en2ne", trust_remote_code=True)
-        self.en2ne_model = AutoModelForSeq2SeqLM.from_pretrained(f"{model_dir}/en2ne", trust_remote_code=True).to(self.device)
+        nllb_path = f"{model_dir}/en2ne"
+        self.nllb_tokenizer = AutoTokenizer.from_pretrained(nllb_path )
+        self.nllb_model = AutoModelForSeq2SeqLM.from_pretrained(nllb_path)
 
-    def translate_en_to_ne(self, text: str) -> str:
-        src_lang = "eng_Latn"
-        tgt_lang = "npi_Deva"
+    def ne_to_en(self, text: str) -> str:
+        self.mbart_tokenizer.src_lang = "ne_NP"
+        inputs = self.mbart_tokenizer(text, return_tensors="pt").to(self.device)
+        generated_ids = self.mbart_model.generate(**inputs, forced_bos_token_id=self.mbart_tokenizer.lang_code_to_id["en_XX"])
+        return self.mbart_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    
+    def en_to_ne(self, text: str) -> str:
+        translator = pipeline(
+        "translation",
+        model=self.nllb_model,
+        tokenizer=self.nllb_tokenizer,
+        src_lang="eng_Latn",
+        tgt_lang="npi_Deva",
+        max_length=200
+    )   
+        result = translator(text)
+        return result[0]["translation_text"]
 
-        batch = self.ip.preprocess_batch([text], src_lang=src_lang, tgt_lang=tgt_lang)
-        inputs = self.en2ne_tokenizer(batch, truncation=True, padding="longest", return_tensors="pt", return_attention_mask=True).to(self.device)
-
-        with torch.no_grad():
-            generated = self.en2ne_model.generate(**inputs, use_cache=False, max_length=256, num_beams=5)
-
-        decoded = self.en2ne_tokenizer.batch_decode(generated, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        return self.ip.postprocess_batch(decoded, lang=tgt_lang)[0]
-
-    def translate_ne_to_en(self, text: str) -> str:
-        src_lang = "npi_Deva"
-        tgt_lang = "eng_Latn"
-
-        batch = self.ip.preprocess_batch([text], src_lang=src_lang, tgt_lang=tgt_lang)
-        inputs = self.ne2en_tokenizer(batch, truncation=True, padding="longest", return_tensors="pt", return_attention_mask=True).to(self.device)
-
-        with torch.no_grad():
-            generated = self.ne2en_model.generate(**inputs, use_cache=False, max_length=256, num_beams=5)
-
-        decoded = self.ne2en_tokenizer.batch_decode(generated, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        return self.ip.postprocess_batch(decoded, lang=tgt_lang)[0]
